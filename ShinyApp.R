f6604ae7589f7921efd373032070dbf86a2d7067
Jack <jfreier@macalester.edu>
2019-11-14 14:01:32 -0600

    ...

--- ShinyApp.R
+++ ShinyApp.R
@@ -2,6 +2,46 @@ library(shiny)
 library(tidyverse)
 library(babynames)
 
+# Read the data into R
+DF <- read_csv("MCM_NFLIS_Data.csv", 
+               col_types = cols(FIPS_Combined = col_character(), 
+                                FIPS_State = col_character()))
+
+# Rename Variables
+DF <- DF %>%
+  rename(
+    Year = YYYY,
+    County = COUNTY,
+    Substance = SubstanceName,
+    Opioid_Reports = DrugReports,
+    Total_Drug_Reports_County = TotalDrugReportsCounty,
+    Total_Drug_Reports_State = TotalDrugReportsState
+  )
+
+# Compute total opioid reports for each year and county
+Annual_County <- DF %>%
+  group_by(Year, FIPS_Combined) %>%
+  summarize(Total_Opioid_Reports_County = sum(Opioid_Reports))
+
+# Compute total opioid reports for each year and state
+Annual_State <- DF %>%
+  group_by(Year, FIPS_State) %>%
+  summarize(Total_Opioid_Reports_State = sum(Opioid_Reports))
+
+# Join opioid totals back to original dataset
+DF <- DF %>%
+  inner_join(Annual_County, by = c("Year", "FIPS_Combined")) %>%
+  inner_join(Annual_State, by = c("Year", "FIPS_State"))
+
+# Compute the proportion of opioid reports
+DF <- DF %>%
+  mutate(Prop_Opioid_Reports_County = Total_Opioid_Reports_County / Total_Drug_Reports_County) %>%
+  mutate(Prop_Opioid_Reports_State = Total_Opioid_Reports_State / Total_Drug_Reports_State)
+
+# Reorder Columns
+DF <- DF[c(1,2,3,4,5,6,7,8,11,12,9,10,13,14)]
+
+
 ui <- fluidPage(
   sliderInput(inputId = "years", label = "Year Range",
               min = 2010, max = 2017, value = c(2010, 2017),sep = ""),
@@ -13,8 +53,8 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$timeplot <- renderPlot({
-    babynames %>% 
-      filter(name == input$name, sex == input$sex) %>% 
+    DF %>% 
+      filter(drug == input$name, sex == input$sex) %>% 
       ggplot() +
       geom_line(aes(x = year, y = n)) +
       scale_x_continuous(limits = input$years) +
